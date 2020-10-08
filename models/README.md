d5c8e8fe041665b86060d0bb804bec6c3f9efe91
Sunny Dhoke <sunnydhoke22@gmail.com>
2020-10-08 11:49:58 +0530

    connection to mongodb
    using mongoose, no schema yet.

---
+++ models/README.md
@@ -0,0 +1,1 @@
+model to store all the urls and information
--- server.js
+++ server.js
@@ -1,16 +1,25 @@
+//web framework
 const express = require("express");
 
-//make basic app
-
+//create basic app
 const app = express();
 
 //setting ejs as view engine
 app.set("view engine", "ejs");
 
+// to connect to database
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/urlShortner", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//get
 // index page with request and response parameters
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+//post
+app.post("/shortUrls", (req, res) => {});
 // start listening on specified port
 app.listen(process.env.PORT || 4567);
